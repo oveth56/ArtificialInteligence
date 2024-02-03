fd3c877de8d355caf451c2f146554ce24145fbbb
oveth56 <oveth56@icloud.com>
2024-02-03 11:32:55 -0600

    BusquedaCiega

---
+++ BusquedaCiega.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+def bfs (grafico, inicio, objetivo):
+    cola = deque ([(inicio, [inicio])])
+
+    while cola:
+        nodo, camino = cola.popleft()
+
+        if nodo == objetivo:
+            return camino
+        
+        for vecino in grafico[nodo]:
+            if vecino not in camino:
+                cola.append((vecino, camino + [vecino]))
+
+
+    return None
+grafo= {
+    'A': ['B','C'],
+    'B': ['A','D','E'],
+    'C': ['A','F','G'],
+    'D': ['B'],
+    'E': ['B','H'],
+    'F': ['C'],
+    'G': ['C'],
+    'H': ['E']
+}
+#inicio
+inicio ='A'
+
+#objetivo
+objetivo = 'H'
+
+#resultado
+resultado = bfs(grafo, inicio, objetivo)
+if resultado:
+    print(f"Camino de {inicio} a {objetivo}: {resultado}")
+else:
+    print(f"No se encontro el objetivo {objetivo}")
+
+
